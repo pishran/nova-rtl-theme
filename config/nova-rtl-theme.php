f1ad77069766c4ee9330b48a271dbf63c3d78f0c
Farid Aghili <faridaghili@hotmail.com>
2019-08-14 07:36:56 +0430

    more rtl elements + font updated

--- config/nova-rtl-theme.php
+++ config/nova-rtl-theme.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'stylesheet' => 'https://cdn.rawgit.com/rastikerdar/vazir-font/v20.1.0/dist/font-face.css',
+    'stylesheet' => 'https://cdn.rawgit.com/rastikerdar/vazir-font/v20.1.1/dist/font-face.css',
 
     'font-family' => 'Vazir, Tahoma, serif',
 
