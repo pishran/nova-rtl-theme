6a01d9031940bc09834969781279b106c5f2b27c
Farid Aghili <faridaghili@hotmail.com>
2019-12-08 01:59:40 +0330

    fix popup menus position

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -4,6 +4,7 @@ var link = document.createElement('link');
 link.rel = 'stylesheet';
 link.type = 'text/css';
 link.href = window.config.nova_rtl_theme.stylesheet;
+head.appendChild(link);
 
 var style = document.createElement('style');
 style.type = 'text/css';
@@ -11,6 +12,19 @@ var css = 'html, button, input, optgroup, select, textarea, .chartist-tooltip, .
     'font-family: ' + window.config.nova_rtl_theme.font_family + ';' +
     '}';
 style.appendChild(document.createTextNode(css));
-
-head.appendChild(link);
 head.appendChild(style);
+
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdowns = document.getElementsByClassName('v-popover ml-auto h-9 flex items-center dropdown-right');
+    if (dropdowns.length >= 1) {
+        var popupId = dropdowns[0].firstChild.getAttribute('aria-describedby');
+
+        var style = document.createElement('style');
+        style.type = 'text/css';
+        var css = '#' + popupId + '{' +
+            'left: 108px !important;' +
+            '}';
+        style.appendChild(document.createTextNode(css));
+        head.appendChild(style);
+    }
+}, false);
