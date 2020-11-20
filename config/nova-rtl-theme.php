c4d3f51c5727010e760ae6a1cdf9ece9aca2fc56
Farid Aghili <faridaghili@hotmail.com>
2020-11-20 19:50:59 +0330

    added locales config to define enabled locales

--- config/nova-rtl-theme.php
+++ config/nova-rtl-theme.php
@@ -2,6 +2,11 @@
 
 return [
 
+    'locales' => [
+        'fa',
+        'ar',
+    ],
+
     'stylesheet' => 'https://cdn.rawgit.com/rastikerdar/vazir-font/v26.0.2/dist/font-face.css',
 
     'font-family' => 'Vazir, Tahoma, sans-serif',
--- src/ThemeServiceProvider.php
+++ src/ThemeServiceProvider.php
@@ -15,18 +15,20 @@ class ThemeServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Nova::serving(function (ServingNova $event) {
-            Nova::provideToScript([
-                'nova_rtl_theme' => [
-                    'stylesheet' => config('nova-rtl-theme.stylesheet'),
-                    'font_family' => config('nova-rtl-theme.font-family'),
-                ],
-            ]);
+        if (in_array(app()->getLocale(), config('nova-rtl-theme.locales'))) {
+            Nova::serving(function (ServingNova $event) {
+                Nova::provideToScript([
+                    'nova_rtl_theme' => [
+                        'stylesheet' => config('nova-rtl-theme.stylesheet'),
+                        'font_family' => config('nova-rtl-theme.font-family'),
+                    ],
+                ]);
 
-            Nova::style('nova-rtl-theme', __DIR__.'/../resources/css/theme.css');
+                Nova::style('nova-rtl-theme', __DIR__.'/../resources/css/theme.css');
 
-            Nova::script('nova-rtl-theme', __DIR__.'/../resources/js/theme.js');
-        });
+                Nova::script('nova-rtl-theme', __DIR__.'/../resources/js/theme.js');
+            });
+        }
 
         $this->publishes([
             __DIR__.'/../config/nova-rtl-theme.php' => config_path('nova-rtl-theme.php'),
